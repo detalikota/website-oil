7ab1718c78964ea681a9f8a1410a6307f11a494e
detalikota <detalikota@gmail.com>
2021-03-10 17:52:49 +0300

    testing bootstrap

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, test
 app_name = 'app'
 urlpatterns = [
     path('', home, name='home'),
+    path('test', test, name='test'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'app/home.html')
 
+def test(request):
+    return render(request, 'app/test.html')
+
