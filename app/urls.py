9743ae0b2a51de5ff5c31ce6794e01829ca6a7f7
detalikota <detalikota@gmail.com>
2021-05-16 21:43:47 +0300

    yeah

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, test, allexamples
+from .views import home, test, allexamples, about
 app_name = 'app'
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
     path('test', test, name='test'),
-    path('test2', allexamples, name='allexamples'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'app/home.html')
 
+def about(request):
+    return render(request, 'app/about.html')
+
 def test(request):
     return render(request, 'app/test.html')
     
