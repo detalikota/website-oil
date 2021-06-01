bc5d84e2e41e10be510c2f6c0d74b1a3a2e25fe5
detalikota <detalikota@gmail.com>
2021-06-01 23:51:22 +0300

    yeah

--- websiteoil/settings.py
+++ websiteoil/settings.py
@@ -18,6 +18,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
