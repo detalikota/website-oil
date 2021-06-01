3fcd266c5d5f502a60e935e8f9217f1b627f7bbc
detalikota <detalikota@gmail.com>
2021-06-01 23:15:57 +0300

    yeah

--- websiteoil/settings.py
+++ websiteoil/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!hy$vn*ebm=#uv*vq)wn)nv2qvg3wsr(2bd%@obufrpt46-4(y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -133,6 +133,3 @@ STATICFILES_STORAGE = '.storage.WhiteNoiseStaticFilesStorage'
 
 
 django_heroku.settings(locals())
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    manifest_strict = False
